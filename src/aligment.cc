f7dfe4eac019d434c67f296dc094082d24544903
Vincent Payet <vincent.payet-labonne@epita.fr>
2020-10-26 16:35:43 +0100

    added function mean()

--- src/aligment.cc
+++ src/aligment.cc
@@ -3,14 +3,17 @@
 Vect3f mean(Points points)
 {
     Vect3f m(0,0,0);
-    size_t s = p.get.size();
+    vec_p v = points.getPoints();
+    size_t s = v.size();
     for (size_t i = 0; i < s; i++)
     {
-       m.x += p[i].x; 
-       m.y += p[i].y; 
-       m.z += p[i].z; 
+       m.x += v[i].x; 
+       m.y += v[i].y; 
+       m.z += v[i].z; 
     }
     m.x /= s;
     m.y /= s;
     m.z /= s;
+    
+    return m;
 }
