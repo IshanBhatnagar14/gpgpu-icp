234fd3bbedf4a72db616ae7db87a3c4a26754e5d
Dorian Vinai <dorian.vinai@epita.fr>
2020-10-26 14:46:24 +0100

    Added clang-format

--- src/points.cc
+++ src/points.cc
@@ -1,7 +1,8 @@
 #include "points.hh"
 
-Point3f::Point3f(float x, float y, float z) {
-  this->x = x;
-  this->y = y;
-  this->z = z;
+Point3f::Point3f(float x, float y, float z)
+{
+    this->x = x;
+    this->y = y;
+    this->z = z;
 }
--- src/points.hh
+++ src/points.hh
@@ -3,14 +3,16 @@
 #include <list>
 
 class Point3f {
-public:
-  Point3f(float x, float y, float z);
-  float x;
-  float y;
-  float z;
+  public:
+    Point3f(float x, float y, float z);
+    float x;
+    float y;
+    float z;
 };
 
 class Points {
-private:
-  std::vector<Point3f> points;
+  private:
+    std::vector<Point3f> points;
+
+  public:
 };
