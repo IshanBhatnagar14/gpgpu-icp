35cd91810af146dff52184d71d891b5d8ca7121d
William Chow <william.chow@epita.fr>
2020-10-28 17:51:14 +0100

    icp: start func

--- src/icp.cc
+++ src/icp.cc
@@ -10,18 +10,21 @@ std::vector<size_t> find_correspondences(Points scene, Points model)
 
     std::vector<size_t> correspondences;
 
-    for (size_t i = 0; i < size_s; i++) {
+    for (size_t i = 0; i < size_s; i++)
+    {
         Vect3f s_point = scene[i];
         float min_dist = LONG_MAX;
         size_t chosen_idx = -1;
 
-        for (size_t j = 0; j < size_m; j++) {
+        for (size_t j = 0; j < size_m; j++)
+        {
             Vect3f m_point = model[j];
             float dist = sqrt(pow(m_point.x - s_point.x, 2) +
                               pow(m_point.y - s_point.y, 2) +
                               pow(m_point.z - s_point.y, 2));
 
-            if (dist < min_dist) {
+            if (dist < min_dist)
+            {
                 min_dist = dist;
                 chosen_idx = j;
             }
@@ -29,4 +32,9 @@ std::vector<size_t> find_correspondences(Points scene, Points model)
         correspondences.push_back(chosen_idx);
     }
     return correspondences;
+}
+
+void apply_alignment()
+{
+
 }
