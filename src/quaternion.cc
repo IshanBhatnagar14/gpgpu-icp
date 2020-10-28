f90458de43b57cb5c35d5259d969ada058cb1105
Antoine Coulon <antoine.coulon@epita.fr>
2020-10-28 18:05:24 +0100

    compile with assignement

--- src/quaternion.cc
+++ src/quaternion.cc
@@ -1,28 +1,28 @@
-#include "computation.hh"
 #include "matrix.hh"
+#include "points.hh"
 
-float getSum(Points Pprime, int P_idx, Points Yprime, int Y_idx)
+float getSum(Points Pprime, size_t P_idx, Points Yprime, size_t Y_idx)
 {
     float sum = 0;
-    for (int i = 0; i < Pprime.size(); i++)
-        sum += Pprime[i][P_idx] * Yprime[i][Yprime];
+    for (size_t i = 0; i < Pprime.size(); i++)
+        sum += Pprime[i][P_idx] * Yprime[i][Y_idx];
 
     return sum;
 }
 
 Matrix getNmatrix(Points Pprime, Points Yprime)
 {
-    s_xx = getSum(Pprime, 1, Yprime, 1);
-    s_xy = getSum(Pprime, 1, Yprime, 2);
-    s_xz = getSum(Pprime, 1, Yprime, 3);
+    float s_xx = getSum(Pprime, 0, Yprime, 0);
+    float s_xy = getSum(Pprime, 0, Yprime, 1);
+    float s_xz = getSum(Pprime, 0, Yprime, 2);
 
-    s_yx = getSum(Pprime, 2, Yprime, 1);
-    s_yy = getSum(Pprime, 2, Yprime, 2);
-    s_yz = getSum(Pprime, 2, Yprime, 3);
+    float s_yx = getSum(Pprime, 1, Yprime, 0);
+    float s_yy = getSum(Pprime, 1, Yprime, 1);
+    float s_yz = getSum(Pprime, 1, Yprime, 2);
 
-    s_zx = getSum(Pprime, 3, Yprime, 1);
-    s_zy = getSum(Pprime, 3, Yprime, 2);
-    s_zz = getSum(Pprime, 3, Yprime, 3);
+    float s_zx = getSum(Pprime, 2, Yprime, 0);
+    float s_zy = getSum(Pprime, 2, Yprime, 1);
+    float s_zz = getSum(Pprime, 2, Yprime, 2);
 
     Matrix N(4);
     N[0][0] = s_xx + s_yy + s_zz;
@@ -31,7 +31,7 @@ Matrix getNmatrix(Points Pprime, Points Yprime)
     N[0][3] = s_xy + s_yx;
 
     N[1][0] = s_yz - s_zy;
-    N[1][1] = s_xx - s_zz - syy;
+    N[1][1] = s_xx - s_zz - s_yy;
     N[1][2] = s_xy + s_yx;
     N[1][3] = s_xz + s_zx;
 
