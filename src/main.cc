06fed08c561ae2bec3d88f235c60f811e948a190
Vincent Payet <vincent.payet-labonne@epita.fr>
2020-10-29 22:19:28 +0100

    main ok jusqua translation

--- src/main.cc
+++ src/main.cc
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 
     l << "Parse OK" << std::endl;
 
-    apply_alignment(scene, model);
+    apply_alignment(model, scene);
 
     l << "find_aligment() OK" << std::endl;
 
