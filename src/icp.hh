612e8191db86d06a907cfa086173111d4a69e460
William Chow <william.chow@epita.fr>
2020-10-29 17:43:51 +0100

    icp: add function

--- src/icp.hh
+++ src/icp.hh
@@ -16,3 +16,4 @@ Matrix get_quaternion_matrix(Points Pprime, Points Yprime);
 Matrix get_rotation_matrix(Matrix q);
 float get_scaling_factor(Points Pprime, Points Yprime);
 Vect3f get_transational_offset(Vect3f mu_p, Vect3f mu_y, float s, Matrix R);
+float residual_error(Points p, Points y, float s, Matrix r, Vect3f t);
